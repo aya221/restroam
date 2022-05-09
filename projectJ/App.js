3255b5ef1ff222b6fab37e991e120fd5a4b2b8ea
ahmed fourati <minatocss@gmail.com>
2022-05-09 18:03:48 +0100

    Addtoilet

--- projectJ/App.js
+++ projectJ/App.js
@@ -1,20 +1,176 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, TextInput, Switch, Button } from 'react-native';
+import TimeSelectArray from './components/TimeSelectArray';
+import { useState } from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
+import TimeSelect from './components/TimeSelect';
 
 export default function App() {
+  var numberi = 0;
+  const [price, setPrice] = useState('0,00€');
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const [privacy, setPrivacy] = useState('Public');
+
+  function toggleSwitch() {
+    setIsEnabled(!isEnabled);
+  }
+
+  function changePrice(v) {
+    setPrice(v + '€');
+    v = v + '€';
+    console.log(price)
+  }
+
+  function changePrivacy(item) {
+    setPrivacy(item);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.containerTitle}>
+        <Text style={styles.cadre}>
+          Add Toilet
+        </Text>
+      </View>
+
+      <View>
+        <Text style={{ paddingLeft: "2%" }}>
+          Opening hours
+        </Text>
+        <View style={{
+          display: "flex",
+          alignItems: 'center'
+        }}>
+
+        </View>
+        <TimeSelectArray></TimeSelectArray>
+        <Text style={{ padding: "2%" }}>
+          Specify its location
+        </Text>
+        <View>
+          <TextInput style={styles.input}
+            placeholder="Address...">
+          </TextInput>
+        </View>
+        <Text style={{ padding: "2%" }}>
+          Indicate its details
+        </Text>
+        <View style={{
+          display: 'flex',
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          marginLeft: '5%'
+        }}>
+          <View style={styles.cadreS}>
+            <Text>Price</Text>
+            <View style={{
+              display: 'flex',
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              marginLeft: '5%',
+              alignItems: 'center'
+            }}>
+              <TextInput style={styles.input}
+                placeholder="0,00"
+                onChangeText={x => changePrice(x)}>
+              </TextInput>
+              <Text style={{ fontSize: 20, }}>
+                €
+              </Text>
+            </View>
+          </View>
+          <View style={styles.cadreS}>
+            <Text>Handicap access</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "lightgreen" }}
+              thumbColor={isEnabled ? "#A5AD8B" : "#EAA0B2"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+              style={{
+                margin: '10%',
+                marginLeft: '35%',
+              }}
+            />
+          </View>
+        </View>
+        <Text style={{ padding: 5 }}>
+          Other Details
+        </Text>
+        <TextInput style={styles.inputDescription}
+          multiline={true}
+          placeholder="Insert description in here...">
+
+        </TextInput>
+        <View
+          style={{ margin: 30 }}>
+          <Button
+            title="Submit"
+            onPress={() => console.log('click')}
+          />
+
+        </View>
+      </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f28d82',
+    justifyContent: 'flex-start',
+
+  },
+  cadre: {
+    borderWidth: 1,
+    backgroundColor: "#fae8e0",
+    padding: "2%",
+    paddingLeft: "35%",
+    paddingRight: "35%",
+    marginTop: "2.5%",
+    borderRadius: 15,
+    fontSize: 20,
+  },
+  cadreS: {
+    borderWidth: 1,
+    backgroundColor: "#fae8e0",
+    padding: "2%",
+    marginTop: "2.5%",
+    borderRadius: 15,
+    fontSize: 150,
+    marginRight: '5%',
+    minWidth: 150
+  },
+  containerTitle: {
+    backgroundColor: '#f28d82',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  addInputs: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    padding: "5%",
+    height: 60,
+    marginLeft: '5%',
+    borderWidth: 1,
+    backgroundColor: 'rgb(250, 235, 215)',
+    borderRadius: 15,
+    marginRight: '5%',
+    minWidth: 20
+  },
+  inputDescription: {
+    padding: "5%",
+    height: 60,
+    marginLeft: '5%',
+    borderWidth: 1,
+    backgroundColor: 'rgb(250, 235, 215)',
+    borderRadius: 15,
+    marginRight: '5%',
+    minWidth: 20,
+    height: 100
   },
 });
