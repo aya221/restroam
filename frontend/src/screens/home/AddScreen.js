13e2c3b1a12d9fe57fbd8ad802c878db3313892c
manuel.scheurich <manuel.scheurich@stud.tu-darmstadt.de>
2022-07-17 14:32:49 +0200

    Linked OutloggedScreen to Add Page

--- frontend/src/screens/home/AddScreen.js
+++ frontend/src/screens/home/AddScreen.js
@@ -1,13 +1,25 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Text, View, StyleSheet, Button, TouchableOpacity, SafeAreaView } from 'react-native';
 //import { TimePicker } from 'react-native-simple-time-picker';
 //import DateTimePicker from '@react-native-community/datetimepicker';
 import AddToilet from '../addPage/AddToilet';
+import { getAsyncStorageItem, setAsyncStorageItem } from '../../util';
 
 
 export default function AddScreen({ navigation }) {
     
+    const [token, setToken] = useState();
+
+        useEffect(() => {
+            getAsyncStorageItem('token')
+            .then((tokenFromStorage) => {
+                if(tokenFromStorage == null) navigation.navigate("Not logged in");
+                else setToken(tokenFromStorage);
+            }).catch(err => console.log(err));
+        }, []);
+
     return(
+        
         
         <View style= {styles.container}>
             <AddToilet navigation={navigation}/>
